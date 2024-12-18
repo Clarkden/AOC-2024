a3f17e4def9dea382f0580c9bc29c41b322e2247
Clarkden <clarkh_@outlook.com>
2024-12-18 02:11:36 -0800

    Add occurrence tracking and similarity calculation to distance computation

--- day-1/main.go
+++ day-1/main.go
@@ -19,6 +19,8 @@ func main() {
 	column1 := []int{}
 	column2 := []int{}
 
+	occurances := make(map[int]int)
+
 	for scanner.Scan() {
 
 		values := strings.Split(scanner.Text(), "   ")
@@ -38,12 +40,23 @@ func main() {
 
 		column1 = append(column1, val1)
 		column2 = append(column2, val2)
+
+		occurances[val1] = 0
+	}
+
+	for _, v := range column2 {
+		if _, found := occurances[v]; found {
+			if found {
+				occurances[v]++
+			}
+		}
 	}
 
 	sort.Ints(column1)
 	sort.Ints(column2)
 
 	distance := 0
+	similarity := 0
 
 	for i, v := range column1 {
 		if v < column2[i] {
@@ -54,5 +67,10 @@ func main() {
 		distance += v - column2[i]
 	}
 
-	log.Print(distance)
+	for k, v := range occurances {
+		similarity += k * v
+	}
+
+	log.Printf("Distance: %d", distance)
+	log.Printf("Similarity: %d", similarity)
 }
